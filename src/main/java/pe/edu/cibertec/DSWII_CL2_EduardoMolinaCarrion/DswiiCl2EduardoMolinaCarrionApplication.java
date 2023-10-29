c45558b675f7f8224e0cc311076e0c12a909fec2
EduardoMolinaC <clausmolina1999@gmail.com>
2023-10-28 21:31:58 -0500

    primer commit

---
+++ src/main/java/pe/edu/cibertec/DSWII_CL2_EduardoMolinaCarrion/DswiiCl2EduardoMolinaCarrionApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_CL2_EduardoMolinaCarrion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl2EduardoMolinaCarrionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl2EduardoMolinaCarrionApplication.class, args);
+	}
+
+}
